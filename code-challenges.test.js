1c83c41a50460f6414f81a4c4678d59a28c62049
Valeriano-WebDev <Valeriano-WebDev@gmail.com>
2022-02-28 05:32:27 -0800

    Week 2 Assessment Completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,59 +11,147 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
-
+// describe("hello", () => {
+//
+//
+//     it("returns a string that says hi", () => {
+//
+//
+//         expect(hello()).toEqual("hi")
+//     })
+// })
+//
 // a) Create a test with expect statements for each of the variables provided.
-
-const numbersArray1 = [6, 7, 8, 9, 10]
+// const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
-
 // b) Create the function that makes the test pass.
 
 
 
-// --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
+// --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisible by three or not.
+/**Pseudo Code
+ - Our first step will be to define our function giving it a name.
+ - It will take a number as an argument, so we can give a name to the parameter it will take in , in this case num
+ - In order to decide whether the argument is divisible by three or not, we will use a conditional if/else statement
+   to evaluate the condition.
+ - We can use the modulo operator to accomplish this task.
+ - Based on our two desired returns/outputs "divisible by three", or "not divisible by three" we will log out the return with one of these
+   phrases.
+ - Running Jest with the correctly defined Describe, It , and Expect statements satisfies our need to log at the end.
+ - Instead , if our function is correct , we will receive all passing green for our Jest tests.
+ **/
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
-// Expected output: "15 is divisible by three"
-const num2 = 0
-// Expected output: "0 is divisible by three"
-const num3 = -7
-// Expected output: "-7 is not divisible by three"
+    describe("divisibleByThree", () => {
 
+    it("returns a string whether the argument passed is divisible by 3", () => {
+
+        const num1 = 15
+//      Expected output: "15 is divisible by three"
+        const num2 = 0
+//      Expected output: "0 is divisible by three"
+        const num3 = -7
+//      Expected output: "-7 is not divisible by three"
+
+        expect(divisibleByThree(num1)).toEqual(`${num1} is divisible by three`)
+        expect(divisibleByThree(num2)).toEqual(`${num2} is divisible by three`)
+        expect(divisibleByThree(num3)).toEqual(`${num3} is not divisible by three`)
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+     const divisibleByThree = (num) => {
+        if(num % 3 === 0 ) {
+            return `${num} is divisible by three`
+        } else {
+            return `${num} is not divisible by three`
+        }
+     }
 
-
+     /** I GOT GREEN!!! **/
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+/**Pseudo Code
+ - Our first step will be to define our function giving it a name.
+ - After clarification with the cohort, we stick to the given output of each word in the array return with the first letter of the word
+   capitalized.
+ - It will take an array as an argument, so we can give a name to the parameter it will take in , in this case arr
+ - We know that we will be iterating through the array and returning an array of the same length, so we can use .map to accomplish this.
+ - The array can be broken down into two parts. The first letter, or character at index[0] utilizing charAt[0] and the remaining letters or substring,
+   utilizing substring[1].
+ - The toUppercase method will be used on the first letter or element and toLowercase will be used on the substring that remains.
+ - This ensures proper capitalization of each element that comprises the string.
+ - Running Jest with the correctly defined Describe, It , and Expect statements satisfies our need to log at the end.
+ - Instead , if our function is correct , we will receive all passing green for our Jest tests and receive the desired return.
+ **/
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+    describe("capitalized", () => {
 
+    it("returns an array with the first letter of each word of the passed in array capitalized.", () => {
+
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+//      Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+//      Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+
+        expect(capitalized(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(capitalized(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
 
 // b) Create the function that makes the test pass.
+    const capitalized = (arr) => {
+        return arr.map(element => {
+            return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase()
+        })
+}
 
-
-
+    /** I GOT GREEN!!! **/
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+/**Pseudo Code
+ - Our first step will be to define our function giving it a name.
+ - The desired output is the index of the first vowel that occurs  of a string that is passed in. We can use string as the placeholder for our parameter.
+ - In order to easily iterate through the string and evaluate each letter against the vowels a,e,i,o,u, the .split("") method will be used.
+ - Afterwards, the .filter will be called in conjunction with the .split method to iterate over the created array of letters.
+ - Using the logical or || and comparison === operators. The function will evaluate each letter against all vowels during every iteration.
+ - Any vowels will be added to our newly created array and we return the first vowel , or vowel at index[0]
+ - Once again, passing green Jest Tests satisfies our need to console.log , as long as the data passed into both the function and initial
+   describe,it,expect statements are correct.
+ **/
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+    describe("vowelIndex", () => {
 
+    it("returns the the first vowel that occurs in a passed in string", () => {
+
+        const vowelTester1 = "learn"
+//      Expected output: 1
+        const vowelTester2 = "academy"
+//      Expected output: 0
+        const vowelTester3 = "challenges"
+//      Expected output: 2
+
+        expect(vowelIndex(vowelTester1)).toEqual(1)
+        expect(vowelIndex(vowelTester2)).toEqual(0)
+        expect(vowelIndex(vowelTester3)).toEqual(2)
+    })
+})
 
 // b) Create the function that makes the test pass.
+
+const vowelIndex = (str) => {
+    let vowelArr = str.split("").filter(vowel => {
+        return vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o"|| vowel === "u"
+    })
+    return str.indexOf(vowelArr[0])
+}
+
+
+/** I GOT GREEN!!! **/
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,80 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-  Your answer:
+  Your answer: 
+               A parameter is a placeholder for the value to passed in when defining a function. An argument would be the actual
+               value/s used when the function is executed. 
 
   Researched answer:
-
+               There are two types of parameters. Input and output parameters. They both represent values that are to
+               be passed in and expressed/returned. 
+**References:** https://en.wikipedia.org/wiki/Parameter_%28computer_programming%29#Parameters_and_arguments
+                https://developer.mozilla.org/en-US/docs/Glossary/Parameter          
 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: 
+               I believe the .map() utilizes a callback function to iterate over an array utilizing the predefined parameters
+               of value, index and a third. The predefined parameter of value will always be used when using the .map() method
 
   Researched answer:
-
+               The third parameter is the array .map() was called on. So we have the value of the elements in the array , the indices 
+               or placement of those elements, and the array which was called on. When we use the .map() the output will be an array
+               of the same length with the modified values.
+  **References:** https://github.com/learn-academy-2022-alpha/Syllabus/blob/main/javascript/higher-order-functions.md
 
 
 3. What is the difference between map and filter?
 
   Your answer:
+              Both will iterate through an array but Map will return an array of the same length, while filter will only
+              return values that meet the given criteria.  
 
   Researched answer:
-
+              Map will modify the value of each element in an array while filter simply looks for elements that are 
+              truthy. We are able to utilize both, chaining both of these higher functions in shortened code, as 
+              opposed to using many lines of loops and conditionals.
+**References:** https://github.com/learn-academy-2022-alpha/Syllabus/blob/main/javascript/higher-order-functions.md
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: 
+              A function, is a code block that the user defines. It will execute a given action on parameters and 
+              outputs desire. A method is a built-in function in the language that are the developers of the language
+              determined would be used the most often. 
 
   Researched answer:
-
+              We can call functions by their assigned name. Methods must be called using the object it is 
+              acting on as well as the methods name. Methods are properties of objects and must be used with ().
+              Using the method without the () will return the function definition instead of executing 
+              the function itself.
+**References:** https://www.w3schools.com/js/js_object_methods.asp
 
 
 5. What is object destructuring?
 
-  Your answer:
+  Your answer: 
+             Object destructuring is where we access specific key value pairs and assign them to a variable.
+             This allows us easier access to the values we need instead of having to . notation and write 
+             redundant code.
 
   Researched answer:
-
-
+             Objects facilitate quicker manipulation and extraction of object properties.
+**References:** https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 
 6. STRETCH: What is hoisting in JavaScript?
 
   Your answer:
+            Hoisting in JavaScript is when either a variable or function definition is able to be utilized outside 
+            its defined scope.  
 
   Researched answer:
-
+               Instead of my answer of outside its defined scope, the variable is "hoisted" or brought to the top of the code block.
+               When defined with var this is an acceptable use case. However, defining a variable using let or const will return an error or fail to run 
+               if it is used before it is declared. 
+**Reference** https://www.w3schools.com/js/js_hoisting.asp
 
 
 ## Looking Ahead: Terms for Next Week
@@ -59,11 +89,29 @@ Then, research the question to expand on your answer. Even if you feel you have 
 Research and define the following terms to the best of your ability.
 
 1. Class Inheritance:
+                    Class inheritance is essentially like cloning down a repository from Github. Extending a class will
+                    inherit all the methods from the one it extends.
+**Reference** https://www.w3schools.com/js/js_class_inheritance.asp
 
-2. React:
+2.React:            Just like methods are built in functions commonly used, React is a JavaScript library that facilitates
+                    quicker development of single page and mobile apps. Its is primarily used for building small componetns
+                    of UI and is an open-sourced library created by Facebook (Meta).
 
-3. React state:
+**Reference** https://reactjs.org/
 
-4. React lifecycle methods:
+3.React state:      It is the present or current state of a component. Meaning that the currently stored properties can be 
+                    viewed. When the object properties are redefined, it must be rendered again to display the updated
+                    component.
 
-5. DOM:
+**Reference** https://www.w3schools.com/react/react_state.asp
+
+4.React lifecycle methods: The React Lifecycle is composed of the 1. Mounting, 2. Updating and 3. Unmounting phase
+                           In other words, when the component is added to the DOM, updated(state changed or manipulated)
+                           and removed from the DOM. 
+
+**Reference** https://www.w3schools.com/react/react_lifecycle.asp
+
+5.DOM:  Stands for Document Object Model is what allows the interaction between the web page and JavaScript allowing 
+        for the manipulation of all elements/styling/ and actions to take place.
+
+**Reference** https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction
